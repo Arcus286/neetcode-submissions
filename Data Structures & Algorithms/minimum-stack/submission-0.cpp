4af99d955c33a0652882f39cff8ffcd25927c3f5
Aqua_Blazer <69086207+Arcus286@users.noreply.github.com>
2026-05-17 08:32:45 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,32 @@
+class MinStack {
+private:
+    stack<int> minStack;
+    stack<int> mainStack;
+public:
+
+    MinStack() {
+       
+    }
+    
+    void push(int val) {
+        mainStack.push(val);
+        if(minStack.empty() || val <= minStack.top()){
+            minStack.push(val);
+        }
+    }
+    
+    void pop() {
+        if(mainStack.top() == minStack.top()){
+            minStack.pop();
+        }
+        mainStack.pop();
+    }
+    
+    int top() {
+        return mainStack.top();
+    }
+    
+    int getMin() {
+        return minStack.top();
+    }
+};
