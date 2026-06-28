49b7360b6a5cb6593a1307cb1f3cb511293b0967
Aqua_Blazer <69086207+Arcus286@users.noreply.github.com>
2026-06-28 08:51:46 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        // int maxProfit = 0;
+        // int best_buy = prices[0];
+        // for(int i = 1;i<prices.size();i++){
+        //     if(best_buy < prices[i]){
+        //         maxProfit = max(maxProfit,prices[i] - best_buy);
+        //     }
+        //     best_buy = min(best_buy,prices[i]);
+        // }
+        // return maxProfit;
+        int left = 0;
+        int right = left + 1;
+        int maxProfit = 0;
+
+        while(right != prices.size()){
+            if(prices[left] > prices[right]){
+                left = right;
+                right++;
+            } else {
+                maxProfit = max(maxProfit,prices[right] - prices[left]);
+                right++;
+            }
+        }
+        return maxProfit;
+    }
+};
